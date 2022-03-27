2629fb4ea17bd992cd0a4e81b7b50e85b67ca19b
p-shum <1111shum123@gmail.com>
2022-03-27 22:03:59 +0700

    fixed method for processing strings in text modifier class

--- src/main/java/model/TextModifier.java
+++ src/main/java/model/TextModifier.java
@@ -2,13 +2,14 @@ package model;
 
 import lombok.NoArgsConstructor;
 
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @NoArgsConstructor
 public class TextModifier {
 
-    private static final String z = "з";
-    private static final String v = "в";
+    private static final int MAX_INDEX = 265;
 
     public String changeText(String text) {
         return changeTextWithoutC(text)
@@ -23,22 +24,14 @@ public class TextModifier {
 
     public String processString(String text) {
 
-        TreeMap<Integer, String> map = new TreeMap<>();
+        List<String> dictionary = new ArrayList<>(List.of("з", "З", "в", "В"));
+        AtomicReference<Integer> mIndex = new AtomicReference<>(MAX_INDEX);
 
-        // need to change that
-        map.put(text.indexOf(z), "Z");
-        map.put(text.indexOf(z.toUpperCase()), "Z");
-        map.put(text.indexOf(v), "V");
-        map.put(text.indexOf(v.toUpperCase()), "V");
+        dictionary.forEach(character -> {if (text.contains(character) && text.indexOf(character) < mIndex.get()) {
+            mIndex.set(text.indexOf(character));} });
 
-        Integer minIndex = map.firstKey();
-        if (map.firstKey() == -1 && map.size() > 1) {
-            minIndex = map.higherKey(map.firstKey());
-        }
-
-        if (minIndex > -1) {
-            return text.replaceFirst(String.valueOf(text.charAt(minIndex)), map.get(minIndex));
-        } else return text;
+        return mIndex.get() < MAX_INDEX ? text.replaceFirst(String.valueOf(text.charAt(mIndex.get())),
+                String.valueOf(text.charAt(mIndex.get())).matches("з|З") ? "Z" : "V") : text;
     }
 
     public String changeOnlyFirstLetter(String text) {
@@ -53,5 +46,4 @@ public class TextModifier {
         stringBuilder.append(processString(text.substring(prevIndex)));
         return stringBuilder.toString();
     }
-
 }
